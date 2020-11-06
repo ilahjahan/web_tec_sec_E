657333d04273113b9ba69e9dff79d62c0f2e3be8
ilahjahan <72750242+ilahjahan@users.noreply.github.com>
2020-11-06 23:37:33 +0600

    Add files via upload

---
+++ Lab task4/header1.php
@@ -0,0 +1,22 @@
+<header>
+            <div class="main">
+                <div class="logo">
+                    
+                </div>
+    
+                <ul id="nav">
+                    <li><a href="#">Login as BoB</a></li>
+                    <li><a href="login.php">logout</a></li>
+                    
+                </ul>
+
+               
+
+                
+        
+             <h1> Livon</h1>
+            
+                
+            </div>
+
+            </header>
