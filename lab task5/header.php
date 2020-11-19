d934ee05aa9144a77eb323a2c1635c2cc71592ee
ilahjahan <72750242+ilahjahan@users.noreply.github.com>
2020-11-20 01:57:02 +0600

    uploaded lab5

---
+++ lab task5/header.php
@@ -0,0 +1,23 @@
+<header>
+            <div class="main">
+                <div class="logo">
+                    <img src="Livon.png">
+                </div>
+    
+                <ul id="nav">
+                    <li><a href="home.php">Home</a></li>
+                    <li><a href="login.php">login</a></li>
+                    <li><a href="registation.php">Registation</a></li>
+                   
+                </ul>
+           
+        
+             <h1> Livon </h1>
+            
+                
+            </div>
+
+            </header>
+
+
+          
